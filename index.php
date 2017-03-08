16d7507604b9bc446734ef395aa7d8c96be0ebe0
monabazzaz <mbazzaz@live.unc.edu>
2017-03-08 00:41:35 -0500

    Made substial progress regarding the style guide

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
     <div class="row">
       <div class="col-md-4"></div>
       <div class="col-md-4">
-        <p>This contact manager application was created for my Spring 2017 MEJO 583 class. To access the github repository for this project as well as samples to my other work, please visit <a href="http://monabazzaz.org/portfolio">my portfolio</a>. Cheers!</p>
+        <p>This contact manager application was created for my Spring 2017 MEJO 583 class. The style guide for this application can be found <a href="style.html">here</a>. To access the github repository for this project as well as samples to my other work, please visit <a href="http://monabazzaz.org/portfolio">my portfolio</a>. Cheers!</p>
       </div>
       <div class="col-md-4"></div>
     </div>
